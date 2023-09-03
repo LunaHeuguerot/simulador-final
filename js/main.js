79335cc84ffa5537b12a1062e66ae7ef8ebde5fb
LunaHeuguerot <lunaheuguerot@gmail.com>
2023-09-02 21:19:26 -0300

    Primer commit

---
+++ js/main.js
@@ -0,0 +1,43 @@
+const equipoLocal = "Argentina";
+const equipoVisitante = "Francia";
+
+let golesLocal = 0;
+let golesVisitante = 0;
+let minutosJugados = 0;
+
+console.log(`Comienza el partido entre ${equipoLocal} y ${equipoVisitante}`);
+
+while (minutosJugados < 90 && golesLocal < 5 && golesVisitante < 5) {
+  const minuto = minutosJugados + 1; 
+  
+  const probabilidadGolLocal = Math.random();
+  const probabilidadGolVisitante = Math.random();
+
+  if (probabilidadGolLocal < 0.02) {
+    golesLocal += 1;
+    console.log(`Gol de ${equipoLocal} en el minuto ${minuto}!`);
+    document.write('<div class="card"><img src="/assets/festejos-gol/gol-arg.webp" alt="gol argentina"><h2 class="texto">¡Gol de Argentina!</h2></div>');
+  } else if (probabilidadGolVisitante < 0.02) {
+    golesVisitante += 1;
+    console.log(`Gol de ${equipoVisitante} en el minuto ${minuto}!`);
+    document.write('<div class="card"><img src="/assets/festejos-gol/gol-francia.webp" alt="gol francia"><h2 class="texto">¡Gol de Francia!</h2></div>');
+    
+  }
+
+  minutosJugados = minuto;
+}
+
+// Determinar el ganador después de que el partido ha terminado
+if (golesLocal > golesVisitante) {
+  console.log(`¡El partido ha terminado! Resultado final: ${equipoLocal} ${golesLocal} - ${golesVisitante} ${equipoVisitante}. ¡${equipoLocal} Campeón del mundo!`);
+
+  document.write('<div class="card"><img src="/assets/campeon/arg-campeon.jpg" alt="argentina campeon"><h2 class="texto">¡Argentina Campeon de Mundo!</h2></div>');
+} else if (golesVisitante > golesLocal) {
+  console.log(`¡El partido ha terminado! Resultado final: ${equipoVisitante} ${golesVisitante} - ${equipoLocal} ${golesLocal}. ¡${equipoVisitante} Campeón del mundo!`);
+
+  document.write('<div class="card"><img src="/assets/campeon/francia-campeon.jpg" alt="francia campeon"><h2 class="texto">¡Francia Campeon de Mundo!</h2></div>');
+} else {
+  // Si se alcanzan los 90 minutos y aún hay empate, jugar tiempo extra u otra forma de desempate.
+  console.log(`Terminó el partido: ${equipoLocal} ${golesLocal} - ${golesVisitante} ${equipoVisitante}. Habrá penales en el Estadio Lusail`);
+  document.write('<div class="card"><img src="/assets/campeon/penales.jpg" alt="francia campeon"><h2 class="texto">¡Habrá penales!</h2></div>');
+}
